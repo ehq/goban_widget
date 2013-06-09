3050cfe9ae6d7214061b64e0c87fea41abf360c8
Lucas Nasif <nasif.lucas@gmail.com>
2013-06-08 18:00:00 -0700

    Add canvas elements for the goban.

--- web/board.dart
+++ web/board.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 void main() {
   new Board("#goban");
@@ -7,15 +8,18 @@ void main() {
 class Board {
   Element container;
 
-  String nextMove = 'black';
-  List drawnStones;
-  List hoveredStones;
   Map<String, Element> layers = {};
+  int boardMargin;
+  int size;
+  double lineGap;
+  double stoneRadius;
 
   Board(container) {
     this.container = query(container);
+    this.boardMargin = 70;
 
     createCanvasElements();
+    calculateStoneSize();
   }
 
   void createCanvasElements() {
@@ -25,9 +29,34 @@ class Board {
       canvasLayer.id = layer;
       canvasLayer.classes.add(layer);
 
-      this.layers[layer] = canvasLayer;
+      layers[layer] = canvasLayer;
 
       container.children.add(canvasLayer);
     }
+
+    positionCanvasElements();
   }
+
+  void positionCanvasElements() {
+    var minWidth = px2Int(container.parent.getComputedStyle().minWidth);
+    var height = px2Int(container.parent.getComputedStyle().height);
+
+    this.size = (max(minWidth, height) - boardMargin);
+
+    var sizePx = size.toString() + "px";
+
+    container.style.width = sizePx;
+
+    for (final layer in layers.keys) {
+      layers[layer].style..height = sizePx
+                         ..width  = sizePx;
+    }
+  }
+
+  void calculateStoneSize() {
+    this.lineGap = this.size / 19;
+    this.stoneRadius = this.lineGap / 2;
+  }
+
+  int px2Int(String style) => int.parse(style.replaceAll("px", ""));
 }
